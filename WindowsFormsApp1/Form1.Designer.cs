d5e7137358f07737ec4db76640a2505aba465a8c
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-13 10:55:38 +0530

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBox1id = new System.Windows.Forms.TextBox();
+            this.textBox2name = new System.Windows.Forms.TextBox();
+            this.textBox3salary = new System.Windows.Forms.TextBox();
+            this.EmpGridView = new System.Windows.Forms.DataGridView();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.EmpGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Emp id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Emp Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Emp Salary";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(74, 344);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(190, 344);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(311, 344);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(293, 70);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(205, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Serach by Id";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // textBox1id
+            // 
+            this.textBox1id.Location = new System.Drawing.Point(112, 73);
+            this.textBox1id.Name = "textBox1id";
+            this.textBox1id.Size = new System.Drawing.Size(163, 22);
+            this.textBox1id.TabIndex = 7;
+            // 
+            // textBox2name
+            // 
+            this.textBox2name.Location = new System.Drawing.Point(112, 127);
+            this.textBox2name.Name = "textBox2name";
+            this.textBox2name.Size = new System.Drawing.Size(163, 22);
+            this.textBox2name.TabIndex = 8;
+            // 
+            // textBox3salary
+            // 
+            this.textBox3salary.Location = new System.Drawing.Point(112, 185);
+            this.textBox3salary.Name = "textBox3salary";
+            this.textBox3salary.Size = new System.Drawing.Size(163, 22);
+            this.textBox3salary.TabIndex = 9;
+            // 
+            // EmpGridView
+            // 
+            this.EmpGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmpGridView.Location = new System.Drawing.Point(449, 108);
+            this.EmpGridView.Name = "EmpGridView";
+            this.EmpGridView.RowHeadersWidth = 51;
+            this.EmpGridView.RowTemplate.Height = 24;
+            this.EmpGridView.Size = new System.Drawing.Size(284, 230);
+            this.EmpGridView.TabIndex = 10;
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(449, 344);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(284, 23);
+            this.btnShowAll.TabIndex = 11;
+            this.btnShowAll.Text = "Show All Employees";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.EmpGridView);
+            this.Controls.Add(this.textBox3salary);
+            this.Controls.Add(this.textBox2name);
+            this.Controls.Add(this.textBox1id);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmpGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBox1id;
+        private System.Windows.Forms.TextBox textBox2name;
+        private System.Windows.Forms.TextBox textBox3salary;
+        private System.Windows.Forms.DataGridView EmpGridView;
+        private System.Windows.Forms.Button btnShowAll;
+    }
+}
+
